ec5cf4f66bc5f48091f188396fbf067990b490e4
Damir Ospanbek <damir.ospanbek@maxinum.co>
2025-11-17 21:54:31 +0500

    fix: deploy configuration fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/Damir_Portfolio' : '',
+  basePath: process.env.GITHUB_REPOSITORY 
+    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` 
+    : process.env.NODE_ENV === 'production' ? '/Damir_Portfolio' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
