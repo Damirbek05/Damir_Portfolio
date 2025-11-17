116c06e5fdf09ff2e9df749288cc383fc0808ad9
Damir Ospanbek <damir.ospanbek@maxinum.co>
2025-11-17 21:44:24 +0500

    feat: add deploy files configurations

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/Damir_Portfolio' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
