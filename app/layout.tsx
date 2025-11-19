5cae8d1a2ed417d1ff8504892dac1a5d04a89201
Damir Ospanbek <damir.ospanbek@maxinum.co>
2025-11-19 21:20:48 +0500

    fix: update website metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Damir',
+  description: 'Created with love',
+  generator: 'dmr',
 }
 
 export default function RootLayout({
